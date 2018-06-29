83b768d74a577b484e9241f770c73a83247625c7
ice cube tray <icecubetray@users.noreply.github.com>
2018-06-29 23:05:23 +0200

    Add result.h

---
+++ result.h
@@ -0,0 +1,23 @@
+#ifndef __SC_RESULT_H
+#define __SC_RESULT_H
+
+
+
+
+#include <stdint.h>
+
+
+
+#define SC_E_SUCCESS						0
+#define SC_E_NULL							1
+#define SC_E_PARAM							2
+#define SC_E_LOCKED							3
+
+
+
+typedef unsigned int sc_result_t;
+
+
+
+
+#endif
