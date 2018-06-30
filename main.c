868692f4b7f5eb20370f2b3a18f119dc26cd3986
ice cube tray <icecubetray@users.noreply.github.com>
2018-06-30 17:39:59 +0200

    Add return code checking

--- main.c
+++ main.c
@@ -45,7 +45,7 @@ main(int argc, char *argv[], char *env[]) {
 
 		if (failed > 0) {
 			puts("tests failed");
-			return 1;
+			return 10;
 		}
 	}
 
@@ -59,11 +59,30 @@ main(int argc, char *argv[], char *env[]) {
 
 	sc_huffman_t huff;
 
-	sc_huffman_init(&huff);
-	sc_huffman_process(&huff, data, strlen(data));
-	sc_huffman_tree_build(&huff);
-	sc_huffman_tree_print(&huff);
-	sc_huffman_clear(&huff);
+	if (sc_huffman_init(&huff) != SC_E_SUCCESS) {
+		perror("Huffman context initialization failed.");
+		return 1;
+	}
+
+	if (sc_huffman_process(&huff, data, strlen(data)) != SC_E_SUCCESS) {
+		perror("Huffman processing failed.");
+		return 2;
+	}
+
+	if (sc_huffman_tree_build(&huff) != SC_E_SUCCESS) {
+		perror("Huffman tree build failed.");
+		return 3;
+	}
+
+	if (sc_huffman_tree_print(&huff) != SC_E_SUCCESS) {
+		perror("Huffman tree print failed.");
+		return 4;
+	}
+
+	if (sc_huffman_clear(&huff) != SC_E_SUCCESS) {
+		perror("Huffman context clearing failed.");
+		return 5;
+	}
 
 
 	return 0;
