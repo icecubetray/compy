8974556e92718a1005b827cf00c6096f494d9d4c
ice cube tray <icecubetray@users.noreply.github.com>
2018-07-08 21:30:59 +0200

    Enhance documentation of `quicksort.h`

--- quicksort.h
+++ quicksort.h
@@ -27,6 +27,13 @@ typedef struct sc_qs_pair {
 extern "C" {
 #endif
 
+	/**
+	 * @brief Quicksorts the specified \p array using the specified \p mode, starting at \p index and processing up to \p size elements.
+	 * @param array The array to sort.
+	 * @param index The index to start at.
+	 * @param size The number of elements to sort.
+	 * @param mode Whether to sort in an ascending manner or descending manner; `#SC_QS_MODE_ASCENDING` or `#SC_QS_MODE_DESCENDING`.
+	 */
 	void sc_quicksort(sc_qs_pair_t *const array, size_t index, const size_t size, const int mode);
 
 #ifdef __cplusplus
