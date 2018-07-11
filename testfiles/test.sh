20c373e5304549637f8525390823999b1ce63f22
ice cube tray <icecubetray@users.noreply.github.com>
2018-07-12 00:51:42 +0200

    Use absolute paths in test.sh

--- testfiles/test.sh
+++ testfiles/test.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-CASE="$@"
+ABS_DIR="$(cd -- "$(dirname -- "$0")" && pwd)";
+CASE_NAME="${@}";
+CASE="${ABS_DIR}/${CASE_NAME}";
 
-../bin/compy -c -i "${CASE}.vec" -o "${CASE}.vec.sca" --log="${CASE}.vec.sca.log"
-../bin/compy -d -i "${CASE}.vec.sca" -o "${CASE}.vec.sca.out" --log="${CASE}.vec.sca.out.log"
+"${ABS_DIR}/../bin/compy" -c -i "${CASE}.vec" -o "${CASE}.vec.sca" --log="${CASE}.vec.sca.log"
+"${ABS_DIR}/../bin/compy" -d -i "${CASE}.vec.sca" -o "${CASE}.vec.sca.out" --log="${CASE}.vec.sca.out.log"
 
 if diff "${CASE}.vec" "${CASE}.vec.sca.out"; then
-	echo success;
+	echo "${CASE_NAME}: successful";
 else
-	echo failed;
+	echo "${CASE_NAME}: failed";
 fi;
