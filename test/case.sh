46cd98402dc62520c2eb69a72acca6c6c8b010fe
ice cube tray <icecubetray@users.noreply.github.com>
2018-07-14 04:20:42 +0200

    Improve test logging

--- test/case.sh
+++ test/case.sh
@@ -4,11 +4,13 @@ ABS_DIR="$(cd -- "$(dirname -- "$0")" && pwd)";
 CASE_NAME="${@}";
 CASE="${ABS_DIR}/files/${CASE_NAME}";
 
+echo -n "${CASE_NAME}...";
+
 "${ABS_DIR}/../bin/compy" -c -i "${CASE}.vec" -o "${CASE}.vec.sca" --log="${CASE}.vec.sca.log"
 "${ABS_DIR}/../bin/compy" -d -i "${CASE}.vec.sca" -o "${CASE}.vec.sca.out" --log="${CASE}.vec.sca.out.log"
 
-if diff "${CASE}.vec" "${CASE}.vec.sca.out"; then
-	echo "${CASE_NAME}: successful";
+if diff "${CASE}.vec" "${CASE}.vec.sca.out" > "${CASE}.vec.diff" 2>&1; then
+	echo " successful!";
 else
-	echo "${CASE_NAME}: failed";
+	echo " failed!";
 fi;
