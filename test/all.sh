bdf833b065e3e825605ee6cfaa0384b784e49384
ice cube tray <icecubetray@users.noreply.github.com>
2018-07-17 01:14:34 +0200

    Fix symlinks not being accepted in test/all.sh

--- test/all.sh
+++ test/all.sh
@@ -4,6 +4,6 @@
 ABS_DIR="$(cd -- "$(dirname -- "$0")" && pwd)";
 
 
-for CASE in `find "${ABS_DIR}/files" -type f -name '*.vec' | sed 's/.*\///' | sed 's/\(.*\)\.vec/\1/'`; do
+for CASE in `find "${ABS_DIR}/files" \( -type f -or -type l \) -name '*.vec' | sed 's/.*\///' | sed 's/\(.*\)\.vec/\1/'`; do
 	"${ABS_DIR}/case.sh" "$CASE"
 done;
